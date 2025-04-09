ffce756c97b1370030aa830f86ec6b1fbb8819a6
suuuujinnnn <suuuujinnnn@gmail.com>
2025-04-09 16:19:18 -0400

    make the button function

--- app/src/main/java/edu/temple/countdownserviceapp/MainActivity.kt
+++ app/src/main/java/edu/temple/countdownserviceapp/MainActivity.kt
@@ -1,20 +1,24 @@
-package edu.temple.countdownserviceapp
-
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btn_start.setOnClickListener {
+            val input = et_countdown.text.toString().toIntOrNull()
+            if (input != null && input > 0) {
+                val intent = Intent(this, CountdownService::class.java).apply {
+                    putExtra("COUNTDOWN_VALUE", input)
+                }
+                startService(intent)
+            } else {
+                Toast.makeText(this, "Please enter a valid number!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
